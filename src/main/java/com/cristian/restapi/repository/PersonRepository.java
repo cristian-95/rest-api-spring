12dc3fb6def8a6301f74b6569a047266fe2740e1
cristian <nem.cristianrc@gmail.com>
2023-12-27 16:16:57 -0300

    Migrations com flyway
    - definição primaria dos endpoints
    - classe de tratamento de erros
    - implementação inicial de service e repository
    - Dozer para mapear de model para vo

---
+++ src/main/java/com/cristian/restapi/repository/PersonRepository.java
@@ -0,0 +1,9 @@
+package com.cristian.restapi.repository;
+
+import com.cristian.restapi.model.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Long> {
+}
