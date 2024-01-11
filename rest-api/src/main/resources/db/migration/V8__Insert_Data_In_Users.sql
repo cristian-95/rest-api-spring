7a041703f7b5b3d6e972de31acc9f613ec92d741
cristian <nem.cristianrc@gmail.com>
2024-01-11 15:37:30 -0300

    mudança de de root do diretório git

---
+++ rest-api/src/main/resources/db/migration/V8__Insert_Data_In_Users.sql
@@ -0,0 +1,3 @@
+﻿INSERT INTO `users` (`user_name`, `full_name`, `password`, `account_non_expired`, `account_non_locked`, `credentials_non_expired`, `enabled`) VALUES
+	('leandro', 'Leandro Costa', '44969d9cd97704d12a77284e2c3ac1f3e26517305cea8d7556400590b3c95faa55d76e8f08c44140', b'1', b'1', b'1', b'1'),
+	('flavio', 'Flavio Costa', '28dae421c5a0857f0d2b8ef8deb7014ff79d63f9786ccb73505f4ad91659b31d8cfaaa51881ef7cd', b'1', b'1', b'1', b'1');
